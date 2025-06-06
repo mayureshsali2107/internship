0f7afb16a69b93eaf24270d2dc0009781b53e27b
mayureshsali210 <you@example.com>
2025-06-06 20:54:49 +0530

    Day 5 Practical Assignment

---
+++ java/Day 5/Practical Assignment/Practical2/src/Practical2/Main.java
@@ -0,0 +1,24 @@
+package Practical2;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		
+		int Marks=sc.nextInt();
+		
+		if((Marks==40)||(Marks>=40)) {
+			System.out.println("Student Is Passed");
+			
+		}
+		else {
+			System.out.println("Student is Fail");
+			
+		}
+		
+		
+
+	}
+
+}
