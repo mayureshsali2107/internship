c19ee29384ce3fa6e345f4b9712bdaf4cfe155d4
mayureshsali2107 <mayureshsali392@gmail.com>
2025-06-06 21:51:01 +0530

    Add files via upload

---
+++ java/Day 5/Home Assignment/Arithmetic_operator.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class Arithmetic_operator {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		// TODO Auto-generated method stub
+		int a=sc.nextInt();
+		int b= sc.nextInt();
+		System.out.println((a+b)+"\n"+(a-b)+"\n"+(a*b)+"\n"+(a/b)+"\n"+(a%b));
+
+	}
+
+}
