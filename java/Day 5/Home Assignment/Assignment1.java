c0792bb8d7591d2a402d3daeb5b51e1bb2e4ebd1
mayureshsali210 <you@example.com>
2025-06-06 20:20:48 +0530

    Assignment! Day 5

---
+++ java/Day 5/Home Assignment/Assignment1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Assignment1{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Number =");
+        int num= sc.nextInt();
+        System.out.println("Square of "+num+"="+(num*num));
+        System.out.println("Cube of "+num+"="+(num*num*num));
+    }
+}
