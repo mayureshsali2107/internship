728a15d4789c93c62cdb7abf50a3519869980712
mayureshsali210 <you@example.com>
2025-06-11 16:28:31 +0530

    Day 8

---
+++ java/Day 8/Practical Assignment/Assignment7.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+
+
+public class Assignment7 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        String s =sc.next();
+
+        switch(s){
+        case "Red":System.out.println("Stop");
+                 break;
+        case "Green":System.out.println("GO");
+                 break;
+        case "Yellow":System.out.println("Ready");
+                 break;
+        
+
+        }
+
+
+    }
+}
