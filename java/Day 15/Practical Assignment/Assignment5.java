5becae8226b0f962463b6c00c742a0969355d440
mayureshsali210 <you@example.com>
2025-06-23 15:43:17 +0530

    Day 15

---
+++ java/Day 15/Practical Assignment/Assignment5.java
@@ -0,0 +1,10 @@
+
+public class Assignment5 {
+    public static void main(String[] args) {
+        int i =10;
+        do { 
+            System.out.println(i);
+            
+        } while (i>0);
+    }    
+}
